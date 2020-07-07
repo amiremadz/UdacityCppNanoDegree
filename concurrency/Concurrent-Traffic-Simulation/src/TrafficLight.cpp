d2472bc02df5826410fbe0c7af31707adeaf4fb0
Amir Emadzadeh <amirok@google.com>
2020-07-06 19:56:21 -0700

    FP. 5a

--- concurrency/Concurrent-Traffic-Simulation/src/TrafficLight.cpp
+++ concurrency/Concurrent-Traffic-Simulation/src/TrafficLight.cpp
@@ -4,15 +4,18 @@
 
 /* Implementation of class "MessageQueue" */
 
-/* 
 template <typename T>
 T MessageQueue<T>::receive()
 {
     // FP.5a : The method receive should use std::unique_lock<std::mutex> and _condition.wait() 
     // to wait for and receive new messages and pull them from the queue using move semantics. 
     // The received object should then be returned by the receive function. 
+    std::unique_lock<std::mutex> lock(_mtx);
+    _condition.wait(lock, [this]() { return !_queue.empty(); });
+    T message = std::move(_queue.front());
+    _queue.pop_front();
+    return message;
 }
-*/
 
 template <typename T>
 void MessageQueue<T>::send(T &&msg)
@@ -20,7 +23,7 @@ void MessageQueue<T>::send(T &&msg)
     // FP.4a : The method send should use the mechanisms std::lock_guard<std::mutex> 
     // as well as _condition.notify_one() to add a new message to the queue and afterwards send a notification.
     std::lock_guard<std::mutex> lock(_mtx);
-    _queue.push_back(std::move(msg));
+    _queue.emplace_back(std::move(msg));
     _condition.notify_one();
 }
 
